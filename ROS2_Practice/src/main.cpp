3c218c27ed6e103fe3c9a003031b205b1db81a0f
asjnsb <asjnsb@gmail.com>
2024-08-24 17:14:32 -0400

    no more errors

--- ROS2_Practice/src/main.cpp
+++ ROS2_Practice/src/main.cpp
@@ -17,11 +17,7 @@ rcl_node_t node;
 rcl_timer_t timer;
 
 // Function for easy error handling when initialzing things
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; 
-  if((remp_rc != RCL_RET_OK)){
-    error_loop();
-  }
-}
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 // Same function but doesn't call the error loop
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
@@ -38,7 +34,7 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time){
   if (timer != NULL) {
     // the message must match the message type of the publisher
     RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data = "Look at me!"
+    sprintf(msg.data.data, "Look at me!");
   }
 }
 
@@ -51,5 +47,34 @@ void setup(){
   allocator = rcl_get_default_allocator();
 
   //create init_options
-  RCCHECK(rclc_support_init(&s))
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "Aidans_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+    "Aidans_node_publisher"));
+  
+  // create timer
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+  
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  sprintf(msg.data.data, "Empty");
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
