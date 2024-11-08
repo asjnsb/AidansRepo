ffbcff9ebe08f30de69a334855b10d6bf6a69a98
asjnsb <asjnsb@gmail.com>
2024-11-08 16:08:41 -0500

    ControllerBegin

---
+++ TeensySwerve/src/controller.cpp
@@ -0,0 +1,8 @@
+// This program is intended to ingest human input and publish a message usable by other nodes.
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_lifecycle/lifecycle_node.hpp>
+
+#include <lifecycle_msgs/msg/state.hpp>
+#include <lifecycle_msgs/msg/transition.h>
+
