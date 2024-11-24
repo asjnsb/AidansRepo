75e3d45d11df5e136872c7f6494c38b9705a6c40
asjnsb <asjnsb@gmail.com>
2024-11-23 20:44:18 -0500

    11/23/2024

---
+++ TeensySwerve/build/tnsy-swrv_controller/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/aidan/GitHub/AidansRepo/TeensySwerve/install/tnsy-swrv_controller'
---
+++ TeensySwerve/build/tnsy_swrv_controller/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/aidan/GitHub/AidansRepo/TeensySwerve/install/tnsy_swrv_controller'
