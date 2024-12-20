35577470f87062b869e48e619b4f606903c3b343
asjnsb <asjnsb@gmail.com>
2024-12-19 23:02:29 -0500

    12/19/24

--- TeensySwerve/src/tnsy_swrv_controller/launch/joystick.launch.py
+++ TeensySwerve/src/tnsy_swrv_controller/launch/joystick.launch.py
@@ -3,6 +3,12 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess, LogInfo, RegisterEventHandler, TimerAction)
+from launch.conditions import IfCondition
+from launch.event_handlers import (OnExecutionComplete, OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown)
+from launch.events import Shutdown
+from launch.substitutions import (EnvironmentVariable, FindExecutable, LaunchConfiguration,  LocalSubstitution, PythonExpression)
+
 def generate_launch_description():
     joy_params = os.path.join(get_package_share_directory('tnsy_swrv_controller'), 'config', 'joystick.yaml')
     
@@ -12,14 +18,31 @@ def generate_launch_description():
         parameters=[joy_params],
     )
     
-    teleop_node = Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        name='teleop_node',
-        parameters=[joy_params]
+    joy_translator = Node(
+        package='tnsy_swrv_controller',
+        executable='joy_translator',
+        name='joy_translator_node'
     )
 
     return LaunchDescription([
         joy_node,
-        teleop_node,
+        joy_translator,
+        
+        RegisterEventHandler(
+            OnProcessExit(
+                target_action=joy_translator,
+                on_exit=[
+                    LogInfo(msg=(EnvironmentVariable(name='USER'), ' used Ctrl-C')),
+                    EmitEvent(event=Shutdown(reason='Ctrl-C'))
+                ]
+            )
+        ),
+        RegisterEventHandler(
+            OnShutdown(
+                on_shutdown=[LogInfo(
+                    msg=['Launch was asked to shutdown: ',
+                         LocalSubstitution('event.reason')]
+                )]
+            )
+        ),
     ])
--- TeensySwerve/src/tnsy_swrv_controller/tnsy_swrv_controller/joy_translator.py
+++ TeensySwerve/src/tnsy_swrv_controller/tnsy_swrv_controller/joy_translator.py
@@ -3,20 +3,33 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 
+
 class MyNode(Node):
       def __init__(self):
             super().__init__("joy_translator_node")
-            self.counter_ = 0
-            self.create_timer(1.0, self.timerCallback)
+            self.joyMsg = Joy
+            self.create_timer(0.1, self.timerCallback)
+            self.create_timer(1, self.loggerCallback)
             self.create_subscription(Joy, 'joy', self.listener_callback, 10)
+            self.get_logger().info("Joy Translator Init Complete")
       
+      def loggerCallback(self):
+            self.get_logger().info(str(self.joyMsg.buttons))
+
       def timerCallback(self):
-            self.get_logger().info("Meowdy " + str(self.counter_))
-            self.counter_ += 1
+            lX = self.joyMsg.axes[0]
+            lY = self.joyMsg.axes[1]
+            rX = self.joyMsg.axes[2]
+            rY = self.joyMsg.axes[3]
+            #self.get_logger().info("Left X: %.2f Left Y: %.2f Right X: %.2f Right Y: %.2f" %(lX, lY, rX, rY))
+            self.get_logger().info("%.2f" %(lX))
       
       def listener_callback(self, msg):
-            joyInput = msg.data
-            self.get_logger().info(joyInput)
+            self.joyMsg = msg
+      
+      
+            
+            
 
 def main(args=None):
       # evevrything between init and shutdown is the node
