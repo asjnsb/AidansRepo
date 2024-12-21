5534db2f26a13914f9da6d0cbad8156fcfdcf69b
asjnsb <asjnsb@gmail.com>
2024-12-20 23:47:29 -0500

    12/20/2024

--- TeensySwerve/src/tnsy_swrv_controller/tnsy_swrv_controller/joy_translator.py
+++ TeensySwerve/src/tnsy_swrv_controller/tnsy_swrv_controller/joy_translator.py
@@ -1,28 +1,74 @@
 #!/usr/bin/env python3
 import rclpy
-from rclpy.node import Node
+from rclpy.lifecycle import State, LifecycleNode, LifecycleState, TransitionCallbackReturn
 from sensor_msgs.msg import Joy
 
+"""
+Got lifecycle node setup properly. dunno why there's a lifecycle publisher but no lifecycle subscriber
+Next steps are to see about on_error and if that can handle ctrl-C
+"""
 
-class MyNode(Node):
+class MyNode(LifecycleNode):
       def __init__(self):
             super().__init__("joy_translator_node")
-            self.joyMsg = Joy
-            self.create_timer(0.1, self.timerCallback)
-            self.create_timer(1, self.loggerCallback)
-            self.create_subscription(Joy, 'joy', self.listener_callback, 10)
-            self.get_logger().info("Joy Translator Init Complete")
+            self.joyMsg = None
+            self.timer_0 = None
+            self.timer_1 = None
+            self.sub_ = None
+            self.get_logger().info("IN constructor")
+            
+      def on_configure(self, state):
+            self.get_logger().info("IN on_configure")
+            self.timer_0 = self.create_timer(0.1, self.timer0Callback)
+            self.timer_1 = self.create_timer(1, self.timer1Callback)
+            self.timer_0.cancel()
+            self.timer_1.cancel()
+            return super().on_configure(state)
       
-      def loggerCallback(self):
-            self.get_logger().info(str(self.joyMsg.buttons))
+      def on_cleanup(self, state):
+            self.get_logger().info("IN on_cleanup")
+            self.destroy_subscription(self.sub_)
+            self.destroy_timer(self.timer_0)
+            self.destroy_timer(self.timer_1)
+            return super().on_cleanup(state)
+      
+      def on_activate(self, state):
+            self.get_logger().info("IN on_active")
+            self.sub_ = self.create_subscription(Joy, 'joy', self.listener_callback, 10)
+            self.timer_1.reset()
+            self.timer_0.reset()
+            # super() is used in order to activate lycycle subs/pubs
+            return super().on_activate(state)
+      
+      def on_deactivate(self, state):
+            self.get_logger().info("IN on_deactivate")
+            self.sub_ = self.destroy_subscription(self.sub_)
+            self.timer_1.cancel()
+            self.timer_0.cancel()
+            # super() is used in order to deactiave lycycle subs/pubs
+            return super().on_deactivate(state)
+      
+      def on_shutdown(self, state):
+            self.get_logger().info("IN on_shutdown")
+            self.sub_ = self.destroy_subscription(self.sub_)
+            self.destroy_timer(self.timer_0)
+            self.destroy_timer(self.timer_1)
+            return super().on_shutdown(state)
+      
+      def timer1Callback(self):
+            if self.joyMsg:
+                  self.get_logger().info(str(self.joyMsg.buttons))
 
-      def timerCallback(self):
-            lX = self.joyMsg.axes[0]
-            lY = self.joyMsg.axes[1]
-            rX = self.joyMsg.axes[2]
-            rY = self.joyMsg.axes[3]
-            #self.get_logger().info("Left X: %.2f Left Y: %.2f Right X: %.2f Right Y: %.2f" %(lX, lY, rX, rY))
-            self.get_logger().info("%.2f" %(lX))
+      def timer0Callback(self):
+            lX, lY, rX, rY = 0.0, 0.0, 0.0, 0.0
+            if self.joyMsg:
+                  lX = self.joyMsg.axes[0]
+                  lY = self.joyMsg.axes[1]
+                  rX = self.joyMsg.axes[2]
+                  rY = self.joyMsg.axes[3]
+                  self.get_logger().info("Left X: %.2f Left Y: %.2f Right X: %.2f Right Y: %.2f" %(lX, lY, rX, rY))
+                  #self.get_logger().info("%.2f" %(lX))
+
       
       def listener_callback(self, msg):
             self.joyMsg = msg
