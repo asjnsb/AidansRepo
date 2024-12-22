aecc53332bc433df48081663cc4ca24fa4a0929d
asjnsb <asjnsb@gmail.com>
2024-12-21 20:35:34 -0500

    12/21/2024

--- TeensySwerve/src/tnsy_swrv_controller/launch/joystick.launch.py
+++ TeensySwerve/src/tnsy_swrv_controller/launch/joystick.launch.py
@@ -1,48 +1,90 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode
+from launch_ros.events.lifecycle import ChangeState
 from ament_index_python.packages import get_package_share_directory
-
-from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess, LogInfo, RegisterEventHandler, TimerAction)
+import launch.events as events
+from launch.actions import (EmitEvent, RegisterEventHandler, LogInfo)
 from launch.conditions import IfCondition
 from launch.event_handlers import (OnExecutionComplete, OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown)
 from launch.events import Shutdown
 from launch.substitutions import (EnvironmentVariable, FindExecutable, LaunchConfiguration,  LocalSubstitution, PythonExpression)
+from launch_ros.event_handlers import OnStateTransition
+import lifecycle_msgs.msg
 
 def generate_launch_description():
     joy_params = os.path.join(get_package_share_directory('tnsy_swrv_controller'), 'config', 'joystick.yaml')
     
-    joy_node = Node(
+    joy_node = LifecycleNode(
         package= 'joy',
         executable= 'game_controller_node',
+        name= 'game_controller_node',
         parameters=[joy_params],
+        namespace='nameSpace1'
     )
     
-    joy_translator = Node(
+    joy_translator_node = LifecycleNode(
         package='tnsy_swrv_controller',
         executable='joy_translator',
-        name='joy_translator_node'
+        name='joy_translator_node',
+        namespace='nameSpace1'
+    )
+
+    # Make joy_translator take the 'configure' transition
+    joy_translator_configure_transition_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=events.matches_action(joy_translator_node),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE
+        )
+    )
+
+    # Make joy_translator take the 'activate' transition
+    joy_translator_activate_transition_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=events.matches_action(joy_translator_node),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE
+        )
     )
 
     return LaunchDescription([
         joy_node,
-        joy_translator,
-        
+        joy_translator_node,
+
         RegisterEventHandler(
-            OnProcessExit(
-                target_action=joy_translator,
-                on_exit=[
-                    LogInfo(msg=(EnvironmentVariable(name='USER'), ' used Ctrl-C')),
-                    EmitEvent(event=Shutdown(reason='Ctrl-C'))
+            OnProcessStart(
+                target_action=joy_translator_node,
+                on_start=[
+                    joy_translator_configure_transition_event
                 ]
             )
         ),
         RegisterEventHandler(
-            OnShutdown(
-                on_shutdown=[LogInfo(
-                    msg=['Launch was asked to shutdown: ',
-                         LocalSubstitution('event.reason')]
-                )]
+            OnStateTransition(
+                target_lifecycle_node=joy_translator_node,
+                goal_state='inactive',
+                entities=[
+                    LogInfo( msg = "'joy_translator_node' reached the 'INACTIVE' state"),
+                    # you could put "joy_node" here in order to only launch that node when joy_translator_node reaches the inactive state
+                    joy_translator_activate_transition_event,
+                ],
+            )
+        ),
+        RegisterEventHandler(
+            OnStateTransition(
+                target_lifecycle_node=joy_translator_node,
+                goal_state='active',
+                entities=[
+                    LogInfo(msg = "'joy_translator_node' reached the 'ACTIVE' state")
+                ]
+            )
+        ),
+        RegisterEventHandler(
+            OnProcessExit(
+                target_action=joy_translator_node,
+                on_exit=[
+                    EmitEvent(event=Shutdown(reason='Ctrl-C'))
+                ]
             )
         ),
     ])
--- TeensySwerve/src/tnsy_swrv_controller/tnsy_swrv_controller/joy_translator.py
+++ TeensySwerve/src/tnsy_swrv_controller/tnsy_swrv_controller/joy_translator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import rclpy
-from rclpy.lifecycle import State, LifecycleNode, LifecycleState, TransitionCallbackReturn
+from rclpy.lifecycle import LifecycleNode
 from sensor_msgs.msg import Joy
 
 """
-Got lifecycle node setup properly. dunno why there's a lifecycle publisher but no lifecycle subscriber
-Next steps are to see about on_error and if that can handle ctrl-C
+I don't know if on_error can handle a keyboard interrupt, but I think I can do it via the launch file
+Try setting up an event handler in the launch file to handle a keyboard interrupt??
 """
 
 class MyNode(LifecycleNode):
@@ -27,17 +27,16 @@ class MyNode(LifecycleNode):
       
       def on_cleanup(self, state):
             self.get_logger().info("IN on_cleanup")
-            self.destroy_subscription(self.sub_)
             self.destroy_timer(self.timer_0)
             self.destroy_timer(self.timer_1)
             return super().on_cleanup(state)
       
       def on_activate(self, state):
-            self.get_logger().info("IN on_active")
+            self.get_logger().info("IN on_activate")
             self.sub_ = self.create_subscription(Joy, 'joy', self.listener_callback, 10)
             self.timer_1.reset()
             self.timer_0.reset()
-            # super() is used in order to activate lycycle subs/pubs
+            # super() is used in order to activate lifecycle publishers
             return super().on_activate(state)
       
       def on_deactivate(self, state):
@@ -45,19 +44,22 @@ class MyNode(LifecycleNode):
             self.sub_ = self.destroy_subscription(self.sub_)
             self.timer_1.cancel()
             self.timer_0.cancel()
-            # super() is used in order to deactiave lycycle subs/pubs
+            # super() is used in order to deactiave lifecycle publishers
             return super().on_deactivate(state)
       
       def on_shutdown(self, state):
             self.get_logger().info("IN on_shutdown")
-            self.sub_ = self.destroy_subscription(self.sub_)
+            self.destroy_subscription(self.sub_)
             self.destroy_timer(self.timer_0)
             self.destroy_timer(self.timer_1)
             return super().on_shutdown(state)
       
-      def timer1Callback(self):
-            if self.joyMsg:
-                  self.get_logger().info(str(self.joyMsg.buttons))
+      def on_error(self, state):
+            self.get_logger().info("IN ON_ERROR")
+            self.destroy_subscription(self.sub_)
+            self.destroy_timer(self.timer_0)
+            self.destroy_timer(self.timer_1)
+            return super().on_error(state)
 
       def timer0Callback(self):
             lX, lY, rX, rY = 0.0, 0.0, 0.0, 0.0
@@ -69,7 +71,10 @@ class MyNode(LifecycleNode):
                   self.get_logger().info("Left X: %.2f Left Y: %.2f Right X: %.2f Right Y: %.2f" %(lX, lY, rX, rY))
                   #self.get_logger().info("%.2f" %(lX))
 
-      
+      def timer1Callback(self):
+            if self.joyMsg:
+                  self.get_logger().info(str(self.joyMsg.buttons))
+
       def listener_callback(self, msg):
             self.joyMsg = msg
       
